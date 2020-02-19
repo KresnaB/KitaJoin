87cc0dbc1c6083003f24fd1b2fbcf2aaec5070ee
ivanetra <ivanekaputra24@gmail.com>
2020-02-19 22:49:03 +0700

    Delete cors from middleware and middlewaregroups and add cors to routemiddleware in Kernel.php file

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -42,10 +41,6 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
-        'cors' => [
-            \Barryvdh\Cors\HandleCors::class,
-        ],
     ];
 
     /**
@@ -66,6 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
