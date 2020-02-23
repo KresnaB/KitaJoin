79d34eb1085a96fee6373b32b9fb39f589bb1bb8
ivanetra <ivanekaputra24@gmail.com>
2020-02-23 19:12:03 +0700

    Delete login process after register in register.vue file

--- resources/js/pages/auth/register.vue
+++ resources/js/pages/auth/register.vue
@@ -181,15 +181,6 @@ export default {
       if (data.status) {
         this.mustVerifyEmail = true
       } else {
-        // Log in the user.
-        const { data: { token } } = await this.form.post('/api/login')
-
-        // Save the token.
-        this.$store.dispatch('auth/saveToken', { token })
-
-        // Update the user.
-        await this.$store.dispatch('auth/updateUser', { user: data })
-
         // Redirect home.
         this.$router.push({ name: 'home' })
       }
