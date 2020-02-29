c95a28b8d3c0430e74c4ed21ff8911e5f2b862b2
ivanetra <ivanekaputra24@gmail.com>
2020-02-29 23:35:18 +0700

    Add route in router.js file

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -21,5 +21,6 @@ export default [
       { path: 'password', name: 'settings.password', component: page('settings/password.vue') }
     ] },
 
-  { path: '*', component: page('errors/404.vue') }
+  { path: '*', component: page('errors/404.vue') },
+  { path: '/profile-details', name: 'profile.details', component: page('profiledetails.vue')}
 ]
