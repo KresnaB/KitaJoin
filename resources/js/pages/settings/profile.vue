c85eba5002658c525997f4812796ef5212d6ffa1
ivanetra <ivanekaputra24@gmail.com>
2020-03-07 21:27:36 +0700

    Update user interface for edit profile feature

--- resources/js/pages/settings/profile.vue
+++ resources/js/pages/settings/profile.vue
@@ -1,11 +1,11 @@
 <template>
   <card class="mt-3">
-    <form @submit.prevent="update" @keydown="form.onKeydown($event)">
+    <form @submit.prevent="update" @keydown="form.onKeydown($event)" method="POST" enctype="multipart/form-data">
       <alert-success :form="form" :message="$t('info_updated')" />
 
       <div class="form-group row">
         <div class="col">
-          <img id="user-photo" class="rounded-circle" :src="url" alt="">
+          <img id="user-photo" class="rounded-circle" :src="person.image" alt="">
         </div>
       </div>
 
@@ -35,7 +35,7 @@
 
       <!-- Name -->
       <div class="row">
-        <label class="col col-form-label font-weight-bold pt-0 pb-0">Full name</label>
+        <label class="col col-form-label font-weight-bold pt-0 pb-0">Full name *</label>
       </div>
       <div class="form-group row">
         <div class="col">
@@ -46,21 +46,49 @@
 
       <hr>
 
-      <!-- Email -->
+      <!-- About -->
       <div class="row">
-        <label class="col-md-3 col-form-label font-weight-bold pt-0 pb-0">{{ $t('email') }}</label>
+        <label class="col col-form-label font-weight-bold pt-0 pb-0">About</label>
       </div>
       <div class="form-group row">
         <div class="col">
-          <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
-          <has-error :form="form" field="email" />
+          <textarea v-model="person.bio" :class="{ 'is-invalid': form.errors.has('about') }" class="form-control" name="about"></textarea>
+          <has-error :form="form" field="about" />
         </div>
       </div>
 
-      <!-- Submit Button -->
+      <hr>
+
+      <!-- Telephone -->
+      <div class="row">
+        <label class="col-md-3 col-form-label font-weight-bold pt-0 pb-0">Telephone</label>
+      </div>
       <div class="form-group row">
-        <div">
-          <v-button :loading="form.busy">
+        <div class="col">
+          <input v-model="person.contact" :class="{ 'is-invalid': form.errors.has('telephone') }" class="form-control" type="number" name="telephone">
+          <has-error :form="form" field="telephone" />
+        </div>
+      </div>
+
+      <hr>
+
+      <!-- Interest -->
+      <div class="row">
+        <label class="col-md-3 col-form-label font-weight-bold pt-0 pb-0">Interest</label>
+      </div>
+      <div class="form-group row">
+        <div class="col">
+          <input v-model="person.interest" :class="{ 'is-invalid': form.errors.has('interest') }" class="form-control" type="text" name="interest">
+          <has-error :form="form" field="interest" />
+        </div>
+      </div>
+
+      <hr class="mb-4">
+
+      <!-- Submit Button -->
+      <div class="row">
+        <div class="col-md col-sm">
+          <v-button :loading="form.busy" @click="update(person)">
             Save
           </v-button>
         </div>
@@ -82,14 +110,14 @@
     color: #B1C1C8;
   }
 
-  input {
+  input, textarea {
     outline: 0;
     border-width: 0 0 0px;
     padding: 0;
     -webkit-text-fill-color: black;
   }
 
-  input:focus {
+  input:focus, textarea:focus {
     box-shadow: none !important;
   }
 </style>
@@ -97,6 +125,7 @@
 <script>
 import Form from 'vform'
 import { mapGetters } from 'vuex'
+import axios from 'axios'
 
 export default {
   scrollToTop: false,
@@ -107,14 +136,17 @@ export default {
 
   data: () => ({
     form: new Form({
+      id: '',
       name: '',
-      email: ''
-    }),
-    url: null
+      about: '',
+      telephone: '',
+      interest: ''
+    })
   }),
 
   computed: mapGetters({
-    user: 'auth/user'
+    user: 'auth/user',
+    person: 'person'
   }),
 
   created () {
@@ -124,16 +156,27 @@ export default {
     })
   },
 
-  methods: {
-    async update () {
-      const { data } = await this.form.patch('/api/settings/profile')
+  mounted() {
+    this.$store.dispatch('fetchPerson', {
+      id: this.form.id
+    })
+  },
 
-      this.$store.dispatch('auth/updateUser', { user: data })
+  methods: {
+    async update (person) {
+      return axios.post('/api/update/' + this.person.id, {image: this.person.image, name: this.person.name, bio: this.person.bio, contact: this.person.contact, interest: this.person.interest})
+      .then(res => {
+        this.$store.dispatch(fetchPerson, {
+          id: this.form.id
+        }).catch((err) => {
+          console.log(err);
+        })
+      })
     },
     onFileChange(e) {
       console.log(e.target.files[0]);
       const file = e.target.files[0];
-      this.url = URL.createObjectURL(file);
+      this.person.image = URL.createObjectURL(file);
     }
   }
 }
