8cd09d9936eee9b8e4773b9b45a76478994d8ba3
ivanetra <ivanekaputra24@gmail.com>
2020-03-08 19:07:44 +0700

    Add laravel router for social auth

--- routes/api.php
+++ routes/api.php
@@ -36,4 +36,6 @@ Route::group(['middleware' => 'guest:api'], function () {
 
     Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
     Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
+    Route::post('sociallogin/{provider}', 'Auth\AuthController@SocialSignup');
+    Route::get('auth/{provider}/callback', 'OutController@index')->where('provider', '.*');
 });
