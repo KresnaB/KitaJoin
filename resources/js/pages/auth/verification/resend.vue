efc09910ba6637752cb45bf61b2289dd78033ad3
ivanetra <ivanekaputra24@gmail.com>
2020-05-24 03:21:05 +0700

    Revert "Update resend user interface"
    This reverts commit 60164fb5a4e843a9d78a7fe0efacba03684f8756.

--- resources/js/pages/auth/verification/resend.vue
+++ resources/js/pages/auth/verification/resend.vue
@@ -1,101 +1,32 @@
 <template>
   <div class="row">
     <div class="col-lg-8 m-auto">
-      <card>
-        <div id="container">
-          <div id="jumbotron" class="jumbotron p-0 mb-0">
-            <!-- Card content -->
-            <div class="card-title mb-0">
-              <form @submit.prevent="send" @keydown="form.onKeydown($event)">
+      <card :title="$t('verify_email')">
+        <form @submit.prevent="send" @keydown="form.onKeydown($event)">
+          <alert-success :form="form" :message="status" />
 
-                <!-- Email -->
-                <div id="form-email" class="form-group">
-                  <label id="enter-email-label" class="col-md-3 col-form-label text-md-right text-center text-secondary p-0">Enter Email</label>
-                  <div>
-                    <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control font-weight-bold" type="email" name="email">
-                    <hr class="mt-0">
-                    <has-error :form="form" field="email" />
-                  </div>
-                </div>
-
-                <!-- Submit Button -->
-                <div id="form-button">
-                  <!-- Button -->
-                  <button id="resend" class="btn btn-dark btn-block btn-lg mb-4 font-weight-bold" type="submit">
-                      SEND
-                  </button>
-                </div>
-                <alert-success class="alert-dark" :form="form" message="We have sent your verification link!" />
-              </form>
+          <!-- Email -->
+          <div class="form-group row">
+            <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
+            <div class="col-md-7">
+              <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
+              <has-error :form="form" field="email" />
             </div>
           </div>
-        </div>
+
+          <!-- Submit Button -->
+          <div class="form-group row">
+            <div class="col-md-9 ml-md-auto">
+              <v-button :loading="form.busy">
+                {{ $t('send_verification_link') }}
+              </v-button>
+            </div>
+          </div>
+        </form>
       </card>
     </div>
   </div>
 </template>
-
-<style scoped>
-  @media screen and (max-width: 1000px) {
-    card {
-      padding: 86px 27px;
-    }
-
-    #jumbotron {
-        background-color: white;
-    }
-
-    #gotologin {
-        border-radius: 4px;
-        font-size: 1rem;
-    }
-
-    .card-title p {
-        font-size: 1rem
-    }
-
-    .row {
-      margin-top: 36px;
-      margin-bottom: 36px;
-    }
-
-    .row > div {
-      padding: 0 36px;
-    }
-
-    #container {
-      margin: 21px;
-    }
-
-    .btn {
-      font-size: 1rem;
-      padding: 10px 16px;
-    }
-
-    #enter-email-label {
-      font-size: 0.875rem;
-    }
-
-    input {
-      outline: 0;
-      border-width: 0 0 0px;
-      padding: 0;
-      -webkit-text-fill-color: black;
-    }
-
-    input:focus{
-      box-shadow: none !important;
-    }
-
-    hr {
-      border: 1px solid #6c757d;
-    }
-
-    #form-email {
-      margin-bottom: 46px;
-    }
-  }
-</style>
 
 <script>
 import Form from 'vform'
