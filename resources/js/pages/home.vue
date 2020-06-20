3b1cfeca6605991a06e80ae2f3f2c99f9c85c63c
ivanetra <ivanekaputra24@gmail.com>
2020-06-21 00:12:45 +0700

    Update display teams feature user interface

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,17 +1,41 @@
 <template>
-  <div id="home-card" class="card-body">  
-    <h6 id="person-heading-xs" class="d-block d-sm-none mb-3">Teams</h6>
-    <h6 id="person-heading" class="d-none d-sm-block">Teams</h6>
-    <card v-for="team in teams" v-if="team.user_id !== user.id" v-bind:key="team.id" class="shadow-sm mb-3">
-      <div class="person-row">
-        <div class="my-auto">
-          <router-link id="person-name" :to="{ name: 'team.details', params: {id: team.id, user_id: team.user_id}}" class="navbar-brand font-weight-bold">
-            {{ team.post_name }}
+  <div>
+    <div id="not-available" class="d-none d-sm-block d-sm-none d-md-block">
+      <div id="not-available-container" class="bg-white text-center pt-3 pb-3">
+        <fa id="laptop" icon="laptop"></fa>
+        <h1 id="oops" class="font-weight-normal">Oops!</h1>
+        <p class="mt-4 font-weight-normal">Sorry, we're only available in mobile right now. We'll let you know as soon as we are!</p>
+      </div>
+    </div>
+    <div id="home-card" class="d-block d-sm-none d-none d-sm-block d-md-none card-body my-28">  
+      <div v-if="teams.length">
+        <h5 id="person-heading-xs" class="d-block d-sm-none mb-23 text-black font-weight-bold">Teams</h5>
+        <h6 id="person-heading" class="d-none d-sm-block">Teams</h6>
+        <card v-for="team in teams" v-if="team.user_id !== user.id" v-bind:key="team.id" class="shadow-sm mb-2 mx-1 background-color-black">
+          <div class="person-row my-4-5">
+            <div class="my-auto">
+              <router-link id="person-name" :to="{ name: 'team.details', params: {id: team.id, user_id: team.user_id}}" class="navbar-brand text-white font-weight-bold">
+                {{ team.post_name }}
+              </router-link>
+            </div>
+          </div>
+        </card>
+      </div>
+      <div v-else class="text-center">
+        <img class="w-284" src="svg/undraw_no_data_qbuo.svg" alt="No Teams Illustration">
+        <div class="mt-5">
+          <h4 class="font-weight-bold">No Team</h4>
+        </div>
+        <div class="mt-12">
+          <p class="text-secondary">There is no team at the moment</p>
+        </div>
+        <div class="mt-44">
+          <router-link class="btn btn-1 btn-circle btn-md" :to="{name: 'create.team'}" tag="button">
+            <fa id="add-button-icon" class="text-white" icon="plus"></fa>
           </router-link>
         </div>
       </div>
-    </card>
-    <div id="SC_TBlock_745311" class="SC_TBlock text-white">loading...</div> 
+    </div>
   </div>
 </template>
 
@@ -23,9 +47,13 @@
     }
   }
 
-  @media (max-width: 1024px) {
-    #person-name, #person-interest {
-      font-size: 4.5vw;
+  @media (max-width: 1000px) {
+    #person-name{
+      font-size: 1.125rem;
+    }
+
+    #add-button-icon {
+      font-size: 24px;
     }
   }
 
@@ -60,14 +88,80 @@
   }
 
   #person-heading, #person-heading-xs{
-    color: white;
     margin-top: 3vh;
     margin-bottom: 4vh;
-    font-size: 5vw;
   }
 
   #person-heading-xs {
     text-align: center;
+    
+  }
+
+  .text-black {
+    color: black !important;
+  }
+
+  .w-284 {
+    width: 284px;
+  }
+
+  .mt-12 {
+    margin-top: 12px;
+  }
+
+  .btn-1 {
+    background-color: #F38704;
+  }
+
+  .btn-circle.btn-md { 
+    width: 50px; 
+    height: 50px; 
+    padding: 7px 10px; 
+    border-radius: 25px; 
+    font-size: 10px; 
+    text-align: center; 
+  }
+
+  .mt-44 {
+    margin-top: 44px;
+  }
+
+  #not-available {
+    padding: 120px;
+  }
+
+  #not-available p {
+    margin-bottom: 36px;
+    font-size: 1.125em;
+    color: #A7A7A7;
+  }
+
+  #not-available-container {
+    padding-left: 80px;
+    padding-right: 80px;
+  }
+
+  #laptop {
+    width: 180px;
+    height: 216px;
+  }
+
+  .my-28 {
+    margin-top: 28px;
+    margin-bottom: 28px;
+  }
+
+  .mb-23 {
+    margin-bottom: 23px;
+  }
+
+  .my-4-5 {
+    margin-top: 4.5px;
+    margin-bottom: 4.5px;
+  }
+
+  .background-color-black {
+    background-color: black;
   }
 </style>
 
